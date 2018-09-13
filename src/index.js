be60a2df9b15c19513bff7762d2a5235e55c403b
Victor Rodzko <vicrodzko@mail.ru>
2018-09-13 15:43:13 +0300

    number of zeros

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  let a = Math.floor(number/5);
+  let b = Math.floor(number/(5*5));
+  let c = Math.floor(number/(5*5*5));
+  let d = Math.floor(number/(5*5*5*5));
+  let e = Math.floor(number/(5*5*5*5*5));
+  let f = Math.floor(number/(5*5*5*5*5*5));
+  let g = Math.floor(number/(5*5*5*5*5*5*5));
+  let h = Math.floor(number/(5*5*5*5*5*5*5*5));
+  let i = Math.floor(number/(5*5*5*5*5*5*5*5*5));
+  let j = Math.floor(number/(5*5*5*5*5*5*5*5*5*5));
+  let k = Math.floor(number/(5*5*5*5*5*5*5*5*5*5*5));
+  let l = Math.floor(number/(5*5*5*5*5*5*5*5*5*5*5*5));
+  let m = Math.floor(number/(5*5*5*5*5*5*5*5*5*5*5*5*5));
+  let n = Math.floor(number/(5*5*5*5*5*5*5*5*5*5*5*5*5*5));
+
+  let resultNull = a+b+c+d+e+f+g+h+i+j+k+l+m+n;
+  return resultNull;
 }
